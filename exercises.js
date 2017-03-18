fb1e9aad9dacdcd962e5621dffcf7598f057fb7b
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-03-17 17:47:28 -1000

    updated statement

--- exercises.js
+++ exercises.js
@@ -39,8 +39,15 @@ An empty object
             
   Console.log the object.
 */
-
-
+var kicks = {};
+kicks.brand = "Nike";
+kicks.color = "white";
+kicks.size = 6;
+kicks.buy = function (){
+  return "Hell Yeah!";
+}
+console.log(kicks);
+console.log(kicks.buy());
 /* 
    Declare a variable named `plainBox` and set its value to be an EMPTY object.
    Next, you will add three properties to this object. 
@@ -48,6 +55,13 @@ An empty object
             - add a property named `size` and set it to a number value between `0` and `20`.
             - add a property named `contents` and set it to be an empty array.
 */
+var plainBox = {};
+plainBox.color = "Pink";
+plainBox.size = 13;
+plainBox.contents = [''];
+
+console.log(plainBox);
+
 
 
 /*
@@ -58,7 +72,15 @@ An empty object
             - add a key named `driver` and set it to be `null`.
             - add a key named `passengers` and set it to be an empty array.
 */
+var stockCar = {
+ model: "Jeep",
+ year: 2013,
+ automaticTransmission: true,
+ driver: null,
+ passengers: [],
 
+}
+console.log(stockCar);
 
 /*
    Add new property inside a function
@@ -75,7 +97,18 @@ An empty object
    the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just 
    the value at `name`, and just the value at `age`.
  */
+var plainPerson = {};
+function buildPerson(person, nameString, age){
 
+person.name = nameString;
+person.age = age;
+return person;
+}
+
+var completePerson = buildPerson(plainPerson, "Shelbie", 21);
+console.log(buildPerson(plainPerson));
+console.log(buildPerson("Shelbie"));
+console.log(buildPerson(21));
 
 /*
    Display values of objects that are inside an array
@@ -101,8 +134,68 @@ An empty object
             =====
             ...
  */
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+]
+function printProcessedOrders(orders){
+for(var i = 0; i<orders.length; i++){
+  console.log("====");
+  console.log("id: " + orders[i].id);
+  console.log("Purchase date: " +orders[i].date);
+  console.log("Purchase total: " +orders[i].total);
 
-
+}
+}
+console.log(printProcessedOrders(arrayOfObjects));
 /*
    Addition with an object
    Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`. 
@@ -115,7 +208,14 @@ An empty object
         Invoke your function and pass in your object, store the result to a variable named sumObjResult and use `console.log` 
         to inspect your results.
 */
+var sumObj = {a: 17, b: 26, result: "undefined"};
+function objectAddition(apple){
+  apple.result = (apple.a + apple.b);
+  return apple;
 
+}
+var sumObjResult = objectAddition(sumObj);
+console.log(sumObjResult);
 
 /*
    Print sum function and add as new key-value
@@ -133,8 +233,10 @@ An empty object
         Invoke this function and pass in your object. Further test by changing the values of the object being passed in or 
         **create more** objects and invoke your function multiple times.
  */
-
-
+function printObj(apple){
+  return apple.a + " + " + apple.b + " = " + apple.result;
+}
+console.log(printObj(sumObj));
 /*
    Putting stuff in `plainBox`
         Declare a function named putInPlainBox and a single parameter which will be an object. Within this function, write a 
